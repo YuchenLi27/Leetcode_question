7596496f1d80f67c25f67e5a218d9d8585f24eb3
YuchenLi27 <yuyuchenchen27@gmail.com>
2025-01-09 18:37:16 -0800

    update leetcode 207

---
+++ BFS/207_course_schedule.py
@@ -0,0 +1,35 @@
+"""
+207 course schedule
+topological, bfs
+"""
+from collections import deque
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[]]):
+        #  build a adj list
+        alist = [[] for _ in range(numCourses)]
+        # maintain the indegree
+        indegree = [0] * numCourses
+        #  remember the count of the course to check the ans
+        seen_cnt = 0
+
+        for course in prerequisites:
+            alist[course[1]].append(prerequisites[course[0]])
+            indegree[course[0]] += 1
+
+        queue = deque()
+        for course_indegree in range(numCourses):
+            if indegree[course_indegree] == 0:
+                queue.append(course_indegree)
+
+                while queue:
+                    ele = queue.popleft()
+                    seen_cnt += 1
+                    for nei in alist[ele]:
+                        indegree[nei] -= 1
+                        if indegree[nei] == 0:
+                            queue.append(nei)
+                return seen_cnt == numCourses
+
+
