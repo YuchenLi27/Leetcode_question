8bbb5e2cc0898480abd5a2ded6eadef43727f63e
YuchenLi27 <yuyuchenchen27@gmail.com>
2025-01-12 16:31:56 -0800

    recursion with recall

---
+++ Heap/1054_bardocdes.py
@@ -0,0 +1,31 @@
+"""
+construct a new array that selected elements from one of each barcodes
+ heap
+"""
+import heapq
+from collections import Counter
+
+
+class Bardocdes:
+    def solution(self, nums):
+        cnt = Counter(nums)
+        heap = []
+        res = []
+        for i in cnt:
+            heapq.heappush(heap, (-cnt[i], i))
+
+        while heap:
+            n = len(heap)
+            if n >= 2:
+                t1, c1 = heapq.heappop(heap)
+                res.append(c1)
+                t2, c2 = heapq.heappop(heap)
+                res.append(c2)
+                if t1 + 1 < 0:
+                    heapq.heappush(heap,(t1 + 1, c1))
+                if t2 + 1 < 0:
+                    heapq.heappush(heap, (t2 + 1, c2))
+            else:
+                t, c = heapq.heappop(heap)
+                res.append(c)
+        return res
--- Parentheses/2116_check_locked_valide_parentheses.py
+++ Parentheses/2116_check_locked_valide_parentheses.py
@@ -29,5 +29,32 @@ class Solution:
             return False
 
         return True
+# this question also could use greedy to explain, we go through the input from left to right AND right to left,
+# if there is a open bracket we add 1, if there is close bracket, we reduce 1, but MUST go both sides.
+
+def can_locked_valide_parentheses(self, s, locked):
+    if len(s) % 2 == 1:
+        return False
+
+    left = 0
+    for i, c in enumerate(s):
+        if locked[i] == "0" or c == "(":
+            left += 1
+        elif left > 0:
+            left -= 1
+        else:
+            return False
+
+    left = 0
+    for i in range(len(s) - 1, -1, -1):
+        if locked[i] == "0" or s[i] == ")":
+            left += 1
+        elif left > 0:
+            left -= 1
+        else:
+            return False
+    return True
+
+
 
 
