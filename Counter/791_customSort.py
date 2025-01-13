830f6c62d183316a93c6e4d06f9bcd9f2f16da49
YuchenLi27 <yuyuchenchen27@gmail.com>
2025-01-12 16:34:25 -0800

    updatae

--- Counter/791_customSort.py
+++ Counter/791_customSort.py
@@ -1,43 +1,10 @@
-"""
-given the two arrys, the ans should follow the order, if the ele in order is before the ele1,
-then in ans, the ele should be located before the ele1
-
-recursion needs get back to check different element
-"""
-from pip._internal.resolution.resolvelib import candidates
-
-
 class Solution:
-    def combine(self, candidates: int, target: int):
-        candidates.sort()
-        if candidates[0] > target:
-            return []
-        return self.helper_func(candidates, target, [])
-
-    # get the current_val and its remain(target - current_val), then to check if there is any ele can make the remain val.
-    # the content of return will be the combination of remain val form candidates list
-    # the fomrat of return is list of list,
-    # return [[2, 2, 3], [3, 5]]
-    # if cannot find any meaningful result, return []
-
-
-    def helper_func(self, nums, goal, comb):
-        res = []
-        if goal == 0:
-            res.append(list(comb))
-            return res
-
-        for ele in range(len(nums)):
-            remain = goal - nums[ele]
-            if remain < 0:
-                continue
-            comb.append(nums[ele])
-            remain_comb = self.helper_func(nums[ele:], remain, comb)
-            res.extend(remain_comb)
-            comb.pop()
-
+    def customSortString(self, order: str, s: str) -> str:
+        res=""
+        for i in order:
+            if i in set(s):
+                res += (i * s.count(i))
+        for i in s:
+            if i not in set(res):
+                res += (i * s.count(i))
         return res
-
-
-if __name__ == '__main__':
-    print(Solution().combine([2,3,5], 8))
