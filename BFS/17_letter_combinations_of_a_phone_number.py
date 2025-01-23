b99654ffb9550a389c79bdc04974e210c41d35e5
YuchenLi27 <yuyuchenchen27@gmail.com>
2025-01-22 21:58:35 -0800

    problem 17 update

--- BFS/17_letter_combinations_of_a_phone_number.py
+++ BFS/17_letter_combinations_of_a_phone_number.py
@@ -7,23 +7,26 @@ Return the answer in any order.
 
 class Solution(object):
     def letterCombinations(self, digits):
-       
+
         dic = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
 
         res = []
         if len(digits) == 0:
             return res
-
-        self.dfs(digits, 0, dic, '', res)
+        self.helper(digits, 0, dic, [], res)
         return res
 
-    def dfs(self, nums, index, dic, path, res):
-        if index >= len(nums):
-            res.append(path)
+    def helper(self, nums, curr, dic, path, res):
+        if len(path) == len(nums):
+            res.append("".join(path))
             return
-        string1 = dic[nums[index]]
-        for ele in string1:
-            self.dfs(nums, index + 1, dic, path + ele, res)
+
+        for ele in dic[nums[curr]]:
+            path.append(ele)
+            self.helper(nums, curr + 1, dic, path, res)
+            path.pop()
+
+
 
 if __name__ == "__main__":
     print(Solution().letterCombinations("23"))
