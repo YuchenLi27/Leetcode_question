9880b0deb3fa1b14cd4b357005afddf72e0ae09d
YuchenLi27 <yuyuchenchen27@gmail.com>
2025-01-26 16:59:38 -0800

    update 33

--- BinarySearch/33_Search_in_rotated_sorted_array.py
+++ BinarySearch/33_Search_in_rotated_sorted_array.py
@@ -5,23 +5,42 @@ class Solution(object):
         right = len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
-            if nums[mid] < nums[-1]:
+            if nums[mid] < nums[right]:
+                right = mid - 1
+            elif nums[mid] > nums[right]:
                 left = mid + 1
             else:
-                right = mid - 1
-            return mid # mid here is index of the point
+                return mid # mid here is index of the point
 
-    # def helper(self, arr, mid, goal):
-    #     left = 0
-    #     right = arr[mid]
-    #     while left <= right:
-    #         middle = (left + right) // 2
-    #         if middle <= goal:
-    #             left = middle + 1
-    #         if middle > goal:
-    #             right = middle - 1
-    #     ret
+    # let us know where does the target locate and then do bs individually
+    def search_which_phase(self, nums, pivot_idx, goal):
+
+        if nums[0] <= goal <= nums[pivot_idx]:
+           res = self.find_target(0, pivot_idx, goal, nums)
+        else:
+            res = self.find_target(pivot_idx + 1, len(nums) - 1, goal, nums)
+        return res
+
+
+    #  help use to find the position of the goal, if it does exist
+    def find_target(self, left, right, goal, nums):
+        while left <= right:
+            middle = (left + right) // 2
+            val = nums[middle]
+            if val < goal:
+                left = middle + 1
+            if val > goal:
+                right = middle - 1
+            if val == goal:
+                return middle
+        return -1
 
 if __name__ == '__main__':
     s = Solution()
     print(s.search([4,5,6,7,0,1,2], 3))
+# if __name__ == '__main__':
+#     s = Solution()
+#     print(s.find_target(0, 3, 5, [4, 5, 6, 7, 0, 1, 2]))
+# if __name__ == '__main__':
+#     s = Solution()
+#     print(s.search_which_phase([4, 5, 6, 7, 0, 1, 2], 3, -2))
