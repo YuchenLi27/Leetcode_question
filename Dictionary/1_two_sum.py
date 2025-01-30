1309e9abe7677756f949bef7e873aafe27ed38ef
YuchenLi27 <yuyuchenchen27@gmail.com>
2025-01-30 00:46:28 -0800

    update

--- Dictionary/1_two_sum.py
+++ Dictionary/1_two_sum.py
@@ -7,7 +7,7 @@ class Solution:
         res = []
         for i in range(len(nums)):
             if target - nums[i] in nums_dict:
-                res.append([i, nums[target - nums[i]]])
+                return i, nums[target - nums[i]]
             else:
                 nums_dict[nums[i]] = i
         return [-1, -1]
