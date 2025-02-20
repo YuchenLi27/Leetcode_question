230bc5593884265be20eb87bae27c9f3f813cd10
YuchenLi27 <yuyuchenchen27@gmail.com>
2025-02-19 23:48:55 -0800

    twitter optimization

--- Design/355_twitter.py
+++ Design/355_twitter.py
@@ -90,17 +90,6 @@ class Twitter:
             for tuple_of_list in list_of_post:
                 all_posts.append(tuple_of_list)
 
-        # while len(all_posts) > 10 and len(newst_heap) < 10:
-        #     heap = []
-        #     for ele in all_posts:
-        #         for tup in ele:
-        #             max_timestamp = tup[1] * -1
-        #     max_heap_tup = (tup[0], max_timestamp)
-        #     update_heap = heapq.heappush((heap,max_heap_tup))
-        #     newst_ten = heapq.heappop(update_heap)
-        #     all_users_posts.append(news_ten)
-
-        print(all_posts)
         minheap = []
         heapq.heapify(minheap)
         for post in all_posts:
@@ -145,8 +134,84 @@ class Twitter:
             if followeeId in self.followers[followerId]:
                 self.followers[followerId].remove(followeeId)
 
-# we also can do improve by use heap directly to compare the post's time
+# we also can do improve by get only 10 users' posts
+class Twitter:
+    def __init__(self):
+        self.posts = dict()
+        self.followers = dict()
+        self.timestamp = 0
+        self.user_lastest_timestamp = dict()
 
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if userId not in self.posts.keys():
+            self.posts[userId] = []  # ini_post = []
+        self.timestamp += 1
+        self.posts[userId].append((tweetId, self.timestamp))
+
+        if userId not in self.user_lastest_timestamp:
+            self.user_lastest_timestamp[userId] = 0
+        self.user_lastest_timestamp[userId] = self.timestamp
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        all_users_posts = []
+        all_posts = []
+        fans_post = []
+        min_user_heap = []
+
+        heapq.heapify(min_user_heap)
+        if userId in self.posts:
+            self_tuple = (self.user_lastest_timestamp[userId], userId)
+            heapq.heappush(min_user_heap, self_tuple)
+        if userId in self.followers:
+            for fan in self.followers[userId]:
+                if fan in self.posts:
+                    fan_time = self.user_lastest_timestamp[fan]
+                    fan_tuple = (fan_time, fan)
+                    if len(min_user_heap) < 10:
+                        heapq.heappush(min_user_heap, fan_tuple)
+                    else:
+                        latest_time = min_user_heap[0][0]
+                        if fan_time > latest_time:
+                            heapq.heappop(min_user_heap)
+                            heapq.heappush(min_user_heap, fan_tuple)
+
+        for user_tuple in min_user_heap:
+            user_id = user_tuple[1]
+            all_users_posts.append(self.posts[user_id])
+
+        for list_of_post in all_users_posts:
+            for tuple_of_list in list_of_post:
+                all_posts.append(tuple_of_list)
+        minheap = []
+        heapq.heapify(minheap)
+        for post in all_posts:
+            switched_tuple = (post[1], post[0])
+            if len(minheap) < 10:
+                heapq.heappush(minheap, switched_tuple)
+            else:
+                oldest_in_ten = minheap[0]
+                if post[1] > oldest_in_ten[0]:
+                    heapq.heappop(minheap)
+                    heapq.heappush(minheap, switched_tuple)
+        res = []
+        while len(minheap) > 0:
+            post = heapq.heappop(minheap)
+            res.append(post[1])
+        reverse_res = []
+        for index in range(len(res)):
+            reverse_res.append(res[len(res) - 1 - index])
+        return reverse_res
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        # poster: follower
+        if followerId not in self.followers.keys():
+            self.followers[followerId] = set()  # fans = set()
+        self.followers[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.followers.keys():
+            if followeeId in self.followers[followerId]:
+                self.followers[followerId].remove(followeeId)
 
 # Your Twitter object will be instantiated and called as such:
 # obj = Twitter()
