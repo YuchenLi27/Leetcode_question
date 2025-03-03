0b9658966a180fa9201f2522ca70fd71b3f837e2
YuchenLi27 <yuyuchenchen27@gmail.com>
2025-03-02 17:23:45 -0800

    linkedlist w/out dummy

--- LinkedList/19_remove_Nth_node _from_end.py
+++ LinkedList/19_remove_Nth_node _from_end.py
@@ -5,10 +5,11 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head, n):
+        # count the length of linked list
         dummy = ListNode(0)
         dummy.next = head
         length = 0
-        first = head
+        first = head # don't change the input easily, you can do copy
         while first is not None:
             length += 1
             first = first.next
@@ -19,4 +20,22 @@ class Solution:
             first = first.next
         first.next = first.next.next
         return dummy.next
+
+    # the second way to fingure out this question is not using dummy
+    def removeNthFromEnd2(self, head, n):
+        slow = head
+        fast = head
+
+        for _ in range(n):
+            fast = fast.next
+
+        if not fast:
+            return head.next
+
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        fast = fast.next.next
+        return head
+
 
